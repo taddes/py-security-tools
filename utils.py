3867e5f621c3c34a377794e8135d304ff1108821
Taddes Korris <taddes.korris@passportinc.com>
2020-10-15 19:29:53 -0400

    timer util

---
+++ utils.py
@@ -0,0 +1,11 @@
+from timeit import default_timer as default_timer
+
+def timefunc(func):
+    def inner(*args, **kwargs):
+        start = timer()
+        results = func(*args, **kwargs)
+        end = timer()
+        message = f'{func.__name__} took {end - start} seconds'
+        print(message)
+        return results
+    return inner
