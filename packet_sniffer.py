39f0f4ba52c29dd74e9589d832276e80f39802b1
Taddes Korris <taddes.korris@passportinc.com>
2020-11-02 17:26:46 -0500

    packet sniffer

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -2,6 +2,12 @@ import scapy.all as s
 from PyInquirer import prompt
 from netifaces import interfaces
 
+"""
+    Split out into interfaces, possibly, scan each one individually
+    Capture a number of packets
+    Filter the interfaces, select specific types of packets
+"""
+
 def main():
     interface_choices = [{'name': interface} for interface in interfaces()]
     questions = [
@@ -10,8 +16,24 @@ def main():
         message='Which of the folloing interfaces do you want to sniff?',
         choices=interface_choices
         ),
+        dict(
+            type='input',
+            name='timeout',
+            message='How long do you want to sniff?'
+        ),
+        dict(
+            type='confirm',
+            name='save',
+            message='Do you want to save the captured packets to a file?'
+        )
     ]
     answers = prompt(questions)
+    results = s.sniff(iface=answers['interfaces'], timeout=int(answers['timeout']))
+    if answers['save']:
+        print('saving')
+    else:
+        results.nsummary()
+        s.wrpcap('capture.pcap', results)
 
 
 if __name__ == "__main__":
